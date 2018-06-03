c99ede4cfd22f5dde65d891f5d91e2b87ea3f819
Rahul Meghlan <rahulmeghlan.official@gmail.com>
2018-06-03 22:54:50 +0530

    Added cors.

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 let createError = require('http-errors');
 let express = require('express');
+let cors = require('cors');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', userDetailsRouter);
 
